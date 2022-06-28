ba555208b72a16c4f776aebe6171e0bcee77f575
Scopytech <46828543+oladipotech@users.noreply.github.com>
2022-06-28 13:44:32 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,10 +1,12 @@
-document.write(
+document.write("<h3>My name is <strong style='color:green'>Yasir kamil</strong></h3>"
 
-  'Yasir kamil',
+);
 
-  '  i am 6ft5',
+document.write("<h3>I am <strong style='color:green'>6ft4</strong> and dark in complexion</h3>"
 
-  ' from Nigeria'
+);
+
+document.write("<h3>i am from <strong style='color:green'>Nigeria</strong></h3>"
 
 );
 
